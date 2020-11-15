e35c2ffb8e0f42415ee362bcd386fe94a4a541d3
Caleb Hopkins <CalebHopkins@outlook.com>
2020-11-14 21:48:46 -0600

    Added script.js, date to header

---
+++ script.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+
+    // Figure out and print the date in the header
+    var setDate = moment().format('dddd, MMMM Do');
+    $("#currentDay").append(setDate);
+
+    var hour = moment().format('HH');
+    console.log(hour);
+    // variable to store and loop through scheduler
+
+    
+
+// })
