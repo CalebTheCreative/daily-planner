0392eaea52ff5f90796ba2bd74a273b95bdaf964
Caleb Hopkins <CalebHopkins@outlook.com>
2020-11-14 23:56:15 -0600

    Updated hours

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,33 @@
 $(document).ready(function(){
-
+    var timeBlocks = $(".time-block");
     // Figure out and print the date in the header
     var setDate = moment().format('dddd, MMMM Do');
     $("#currentDay").append(setDate);
 
     // Figure out the current hour of the day
-    var hour = parseInt(moment().format('H'));
-    console.log(hour);
-    
-    function saveEvents(){
-        localStorage.
+    var currentHour = parseInt(moment().format('H'));
+    console.log(currentHour);
+
+    function timeRel(){
+        blockHourEl = parseInt(blockHour);
+        if (moment().format('HH') === data-hour) {
+            console.log(blockHourEl);
+            // set class to present
+            console.log("Present");
+        }
+
+        if (moment().format('HH') < data-hour) {
+            console.log(blockHourEl);
+            // set class to future
+            console.log("Future");
+        }
+
+        if (moment().format('HH') === data-hour) {
+            console.log(blockHourEl);
+            // Set class to past
+            console.log("Past");
+        }
     }
-    
-    scheduleEvents = [];
+    timeRel();
+
 })
